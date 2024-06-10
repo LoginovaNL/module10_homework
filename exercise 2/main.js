fdf669864bbc38adbc02090f5e807bff1f6c0452
Natalia Loginova <161389945+LoginovaNL@users.noreply.github.com>
2024-06-10 11:05:32 +0300

    Update main.js

--- exercise 2/main.js
+++ exercise 2/main.js
@@ -1,4 +1,5 @@
 const btn = document.querySelector('.btn');
+
 function btnClik() {
     alert('Ширина экрана: ' + window.screen.width + 'px\n' + 'Высота экрана: ' + window.screen.height + 'px');
 }
